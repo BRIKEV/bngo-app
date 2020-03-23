f8671edc6248cfcdcff095ac59451dd255027ea1
kevin Martínez <kevinccbsg@gmail.com>
2020-03-24 00:21:44 +0100

    chore: add backend scaffolding

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+# bngo-app
+
+bngo-app
---
+++ backend/components/routes/admin-routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const expressSwaggerGenerator = require('express-swagger-generator');
+
+module.exports = () => {
+  const start = async ({ manifest = {}, app, config }) => {
+    const { swaggerOptions } = config;
+    const expressSwagger = expressSwaggerGenerator(app);
+    const options = {
+      swaggerDefinition: {
+        ...swaggerOptions.swaggerDefinition,
+      },
+      basedir: __dirname,
+      files: ['./**/**-routes.js'],
+    };
+    expressSwagger(options);
+
+    app.use(express.static(config.frontPath));
+    /**
+		 * This endpoint serves the manifest
+		 * @route GET /__/manifest
+		 * @group Admin - Everything about admin routes
+		 * @returns 200 - Sucessful response
+		*/
+    app.get('/__/manifest', (req, res) => res.json(manifest));
+
+    return Promise.resolve();
+  };
+
+  return { start };
+};
