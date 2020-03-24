474c28c474dcfa8c9c95f99bfbc39f03f9da177f
Briam Martinez <briammartinez@gmail.com>
2020-03-24 18:36:06 +0100

    Created Board, ImageCard and OutputImages components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Board } from './Board/Board.vue';
+export { default as ImageCard } from './ImageCard/ImageCard.vue';
+export { default as OutputImages } from './OutputImages/OutputImages.vue';
