b0cc1624f2ce38b68348275a5a11caa5febdd32c
Briam Martinez <briammartinez@gmail.com>
2020-03-24 23:48:21 +0100

    Created initial structure of layout

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,26 +1,47 @@
 <template>
   <div class="dashboard">
     <h1>Dashboard</h1>
-    <Board
-      :numOfColumns="4"
-      :numOfRows="4"
-      :images="images"
-      />
+    <div class="content">
+      <div class="outputImagesContainer">
+        <Board
+          class="outputImages"
+          :numOfColumns="7"
+          :numOfRows="7"
+          :images="images"
+          />
+      </div>
+        <div class="Info">
+          <ImageCard
+            class="imageCard"
+            :imageUrl="imageUrl"
+          />
+          <div class="boardContainer">
+            <Board
+              class="Board"
+              :numOfColumns="4"
+              :numOfRows="4"
+              :images="images.slice(0, 16)"
+            />
+          </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { Board } from '@/components';
+import { Board, ImageCard } from '@/components';
 import mock from '../components/Board/mock';
 
 export default {
   name: 'Dashboard',
   components: {
     Board,
+    ImageCard,
   },
   data() {
     return {
       images: mock,
+      imageUrl: 'https://i.pinimg.com/originals/90/9d/d6/909dd61d3ea0213e274610c7e658c769.png',
     };
   },
 };
@@ -28,5 +49,30 @@ export default {
 <style lang="scss" scoped>
 .dashboard {
   height: 100vh;
+  width: 95%;
+  margin: 0 auto;
+}
+.content {
+  display: flex;
+  .outputImagesContainer {
+    width: 60%;
+    height: 75vh;
+    .outputImages {
+      width: 100%;
+    }
+  }
+  .Info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin: 0 auto;
+    width: 30%;
+    .imageCard {
+      margin: 0 auto;
+    }
+    .boardContainer {
+      height: 50%;
+    }
+  }
 }
 </style>
