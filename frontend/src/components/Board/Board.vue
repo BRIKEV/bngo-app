9e7364933a0b81e3c6e7e1d9c069cbef8eefa47c
Briam Martinez <briammartinez@gmail.com>
2020-03-24 18:44:32 +0100

    Initial components

---
+++ frontend/src/components/Board/Board.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="board">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Board',
+};
+</script>
---
+++ frontend/src/components/ImageCard/ImageCard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="imageCard"></div>
+</template>
+
+<script>
+export default {
+  name: 'ImageCard',
+};
+</script>
