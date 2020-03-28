f2dd23891d121e49457b0fdff14a41bb8b22728c
kevin Martínez <kevinccbsg@gmail.com>
2020-03-28 13:55:50 +0100

    add: chore methods and swagger-validator

--- backend/components/routes/admin-routes.js
+++ backend/components/routes/admin-routes.js
@@ -1,27 +1,22 @@
+const validator = require('swagger-endpoint-validator');
+const bodyParser = require('body-parser');
 const express = require('express');
-const expressSwaggerGenerator = require('express-swagger-generator');
 
 module.exports = () => {
   const start = async ({ manifest = {}, server: { app }, config }) => {
     const { swaggerOptions } = config;
-    const expressSwagger = expressSwaggerGenerator(app);
-    const options = {
-      swaggerDefinition: {
-        ...swaggerOptions.swaggerDefinition,
-      },
-      basedir: __dirname,
-      files: ['./**/**-routes.js'],
-    };
-    expressSwagger(options);
-
     app.use('/test', express.static(config.testPath));
     app.use(express.static(config.frontPath));
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    validator.init(app, swaggerOptions);
     /**
-		 * This endpoint serves the manifest
-		 * @route GET /__/manifest
-		 * @group Admin - Everything about admin routes
-		 * @returns 200 - Sucessful response
-		*/
+     * This endpoint serves the manifest
+     * @route GET /__/manifest
+     * @group Admin - Everything about admin routes
+     * @returns 200 - Sucessful response
+    */
     app.get('/__/manifest', (req, res) => res.json(manifest));
 
     return Promise.resolve();
--- backend/components/routes/index.js
+++ backend/components/routes/index.js
@@ -1,8 +1,11 @@
 const System = require('systemic');
 const adminRoutes = require('./admin-routes');
+const apiRoutes = require('./api-routes');
 
 module.exports = new System({ name: 'routes' })
   .add('routes.admin', adminRoutes())
   .dependsOn('config', 'logger', 'server', 'middleware.prepper', 'manifest')
+  .add('routes.api', apiRoutes())
+  .dependsOn('logger', 'server', 'middleware.prepper')
   .add('routes')
-  .dependsOn('routes.admin');
+  .dependsOn('routes.admin', 'routes.api');
