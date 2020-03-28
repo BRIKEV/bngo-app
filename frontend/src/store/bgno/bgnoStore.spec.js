1756d9a4449cfbd57b5f64fa232681209f0b8900
Briam Martinez <briammartinez@gmail.com>
2020-03-29 00:28:29 +0100

    Added store of bgno and its tests

---
+++ frontend/src/store/bgno/bgnoStore.spec.js
@@ -0,0 +1,32 @@
+import { initialState, mutations } from './bgnoStore';
+
+describe('Tickets store', () => {
+  const state = { ...initialState };
+
+  it('set board correctly', () => {
+    const mockIBoard = ['board1', 'board2'];
+    expect(state.board).toEqual(initialState.board);
+    mutations.SET_BOARD(state, mockIBoard);
+    expect(state.board).toEqual(mockIBoard);
+  });
+
+  it('set user board correctly', () => {
+    const mockUserBoard = ['item1', 'item2'];
+    expect(state.userBoard).toEqual(initialState.userBoard);
+    mutations.SET_USER_BOARD(state, mockUserBoard);
+    expect(state.userBoard).toEqual(mockUserBoard);
+  });
+
+  it('set current result correctly', () => {
+    const mockCurrentResult = {
+        animate: false,
+        selected: {
+          image: 'image.png',
+          description: 'Name of image',
+        },
+    }
+    expect(state.currentResult).toEqual(initialState.currentResult);
+    mutations.SET_CURRENT_RESULT(state, mockCurrentResult);
+    expect(state.currentResult).toEqual(mockCurrentResult);
+  });
+});
