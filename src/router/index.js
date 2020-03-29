a7e060daf92f17f4cc6735c6df980f00e1ac2da9
Briam Martinez Escobar <briammartinez@gmail.com>
2020-03-29 19:07:44 +0200

    Feature/auth (#11)
    * Added persistence utils and js-cookie
    
    * Added access management with vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,19 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import JoinGame from '@/views/JoinGame.vue';
 import Dashboard from '@/views/Dashboard.vue';
+import { hasAccess } from '@/persistence/access';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    redirect: '/join-game',
-  },
-  {
-    path: '/join-game',
     name: 'JoinGame',
     component: JoinGame,
+    meta: {
+      public: true,
+      onlyWhenLoggedOut: true,
+    },
   },
   {
     path: '/dashboard',
@@ -25,6 +26,25 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isPublic = to.matched.some((record) => record.meta.public);
+  const onlyWhenLoggedOut = to.matched.some((record) => record.meta.onlyWhenLoggedOut);
+  const loggedIn = !!hasAccess();
+
+  if (!isPublic && !loggedIn) {
+    return next({
+      path: '/',
+      // query: {redirect: to.fullPath}  // Store the full path to redirect the user to after login
+    });
+  }
+
+  if (loggedIn && onlyWhenLoggedOut) {
+    return next('/dashboard');
+  }
+
+  return next();
 });
 
 export default router;
