f0a73660a5e8054e4d745c70ffee894b62dea7b2
Briam Martinez <briammartinez@gmail.com>
2020-03-29 19:06:12 +0200

    Added access management with vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import JoinGame from '@/views/JoinGame.vue';
 import Dashboard from '@/views/Dashboard.vue';
+import { hasAccess } from '@/persistence/access';
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,10 @@ const routes = [
     path: '/',
     name: 'JoinGame',
     component: JoinGame,
+    meta: {
+      public: true,
+      onlyWhenLoggedOut: true,
+    },
   },
   {
     path: '/dashboard',
@@ -21,6 +26,25 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isPublic = to.matched.some((record) => record.meta.public);
+  const onlyWhenLoggedOut = to.matched.some((record) => record.meta.onlyWhenLoggedOut);
+  const loggedIn = !!hasAccess();
+
+  if (!isPublic && !loggedIn) {
+    return next({
+      path: '/',
+      // query: {redirect: to.fullPath}  // Store the full path to redirect the user to after login
+    });
+  }
+
+  if (loggedIn && onlyWhenLoggedOut) {
+    return next('/dashboard');
+  }
+
+  return next();
 });
 
 export default router;
