ae9747feefddcbe9067ea8ee32721e7551bcba14
Briam Martinez <briammartinez@gmail.com>
2020-03-29 04:42:59 +0200

    Added sections into JoinGame view

--- frontend/src/views/JoinGame.vue
+++ frontend/src/views/JoinGame.vue
@@ -1,16 +1,63 @@
 <template>
   <div class="about">
-    <JoinGameFormSection />
+    <BkForm title="ENTOURAGE BINGO">
+      <transition name="fade" mode="out-in">
+        <template v-if="!log && !create">
+          <JoinGameSection
+            @onAccess="log = true"
+            @onCreateRoom="create = true"
+          />
+        </template>
+        <template v-if="log">
+          <AccessGameForm class="createGameSection">
+            <BkButton
+              class="btn"
+              slot="optional"
+              @btn-clicked="handleAccessClick"
+            >
+              Crea
+            </BkButton>
+          </AccessGameForm>
+        </template>
+        <template v-if="create">
+          <CreateGameForm class="createGameSection">
+            <BkButton
+              class="btn"
+              slot="optional"
+              @btn-clicked="handleCreateClick"
+            >
+              Crea
+            </BkButton>
+          </CreateGameForm>
+        </template>
+      </transition>
+    </BkForm>
   </div>
 </template>
 
 <script>
-import { JoinGameFormSection } from '@/sections';
+import { JoinGameSection, AccessGameForm, CreateGameForm } from '@/sections';
 
 export default {
   name: 'JoinGame',
   components: {
-    JoinGameFormSection,
+    JoinGameSection,
+    AccessGameForm,
+    CreateGameForm,
+  },
+  data() {
+    return {
+      log: false,
+      create: false,
+    };
+  },
+  methods: {
+    handleCreateClick() {
+      console.log('Create');
+    },
+    handleAccessClick() {
+      this.$router.push({ name: 'Dashboard' });
+    },
   },
 };
 </script>
@@ -18,6 +65,13 @@ export default {
 @import "@/theme/index.scss";
 $gradientColor: linear-gradient(90deg, rgb(108, 173, 125) 0%,
   rgb(52, 134, 89) 35%, rgb(108, 173, 125) 100%);
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 
 .about {
   width: 100%;
@@ -27,5 +81,12 @@ $gradientColor: linear-gradient(90deg, rgb(108, 173, 125) 0%,
   align-items: center;
   background: rgb(2,0,36);
   background: $gradientColor;
+}
+.createGameSection {
+  width: 90%;
+  margin: 0 auto;
+}
+.btn {
+  width: 100%;
 }
 </style>
