2d5afcb1a37dab8d8b248fffb752d8048ed8b6d6
Briam Martinez <briammartinez@gmail.com>
2020-03-29 13:51:28 +0200

    Added unit test of BkForm component

---
+++ frontend/src/components/BkForm/BkForm.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount } from '@vue/test-utils';
+import BkForm from '@/components/BkForm/BkForm.vue';
+
+describe('BkForm component', () => {
+  it('renders props.title when passed', () => {
+    const title = 'New title';
+    const wrapper = shallowMount(BkForm, {
+      propsData: { title },
+    });
+    expect(wrapper.text()).toMatch(title);
+  });
+  it('Should show a header when passed props.hasHeader', () => {
+    const wrapper = shallowMount(BkForm, {
+      propsData: {
+        hasHeader: true,
+      },
+    });
+    expect(wrapper.contains('.header')).toBe(true);
+  });
+  it('Should render an icon and iconText when passed props.hasHeader', () => {
+    const wrapper = shallowMount(BkForm, {
+      propsData: {
+        hasHeader: true,
+        headerIcon: 'keyboard_arrow_down',
+        iconText: 'RETURN',
+      },
+    });
+    expect(wrapper.find('.icon').text()).toBe('keyboard_arrow_down');
+    expect(wrapper.find('.iconText').text()).toBe('RETURN');
+  });
+});
