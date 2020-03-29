3dc4e3b717b8edb01b78ac3800feff4338ec4e2d
kevin Martínez <kevinccbsg@gmail.com>
2020-03-29 17:04:57 +0200

    fix configuration in prod environment

--- config/production.js
+++ config/production.js
@@ -1,16 +1,2 @@
-const { join } = require('path');
 
-module.exports = {
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000,
-  },
-  routes: {
-    api: {
-      frontMainFile: join(__dirname, '..', 'frontend', 'dist', 'index.html'),
-    },
-    admin: {
-      frontPath: join(__dirname, '..', 'frontend', 'dist'),
-    },
-  },
-};
+module.exports = {};
