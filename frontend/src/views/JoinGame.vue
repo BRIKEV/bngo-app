b95026c247f8ec3e214c59e68b67f88148a207b0
Briam Martinez <briammartinez@gmail.com>
2020-03-29 03:06:52 +0200

    Created JoinGame view

---
+++ frontend/src/views/JoinGame.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <JoinGameFormSection />
+  </div>
+</template>
+
+<script>
+import { JoinGameFormSection } from '@/sections';
+
+export default {
+  name: 'JoinGame',
+  components: {
+    JoinGameFormSection,
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/theme/index.scss";
+$gradientColor: linear-gradient(90deg, rgb(108, 173, 125) 0%,
+  rgb(52, 134, 89) 35%, rgb(108, 173, 125) 100%);
+
+.about {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgb(2,0,36);
+  background: $gradientColor;
+}
+</style>
