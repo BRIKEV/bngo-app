7061d789141d0a9d6f82dceb83e5fc0d1c28029f
kevin Martínez <kevinccbsg@gmail.com>
2020-03-29 15:10:31 +0200

    chore: created docker image

--- backend/config/default.js
+++ backend/config/default.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 module.exports = {
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4000,
+    port: process.env.PORT || 3000,
   },
   routes: {
     api: {
