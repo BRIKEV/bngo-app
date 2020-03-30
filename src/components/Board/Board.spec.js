b8a4e47d3aaabd1eb8cad4e347a508eba6e81bca
Briam Martinez <briammartinez@gmail.com>
2020-03-31 00:01:56 +0200

    Added new unit test into Board

--- src/components/Board/Board.spec.js
+++ src/components/Board/Board.spec.js
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import { Board } from '@/components';
 
 describe('Board component', () => {
@@ -12,5 +12,29 @@ describe('Board component', () => {
   it('Should be render correctly', () => {
     const wrapper = mount(Board, defaultConfig);
     expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('Should render a disabled class if image is not selected', () => {
+    const wrapper = shallowMount(Board, {
+      propsData: {
+        images: [{
+          image: 'url',
+          selected: false,
+        }],
+      },
+    });
+    expect(wrapper.find('.disabled').exists()).toBe(true);
+  });
+
+  it('Should not render a disabled class if allSelected is true', () => {
+    const wrapper = shallowMount(Board, {
+      propsData: {
+        allSelected: true,
+        images: {
+          selected: false,
+        },
+      },
+    });
+    expect(wrapper.find('.disabled').exists()).toBe(false);
   });
 });
