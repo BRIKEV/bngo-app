c9bd1f1a7b50098fb7e18a0d73303664cc373218
Briam Martinez <briammartinez@gmail.com>
2020-04-01 01:40:19 +0200

    Created Canvas component

---
+++ src/components/Canvas/Canvas.vue
@@ -0,0 +1,76 @@
+<template>
+    <canvas
+      ref="myCanvas"
+      :class="className"
+      :height="height"
+      :width="width"
+      @mouseup="mousePressed = false;"
+      @mouseleave="mousePressed = false"
+      @mousedown="handleMouseDown"
+      @mousemove="handleMouseMove"
+    />
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'Canvas',
+  props: {
+    className: VueTypes.string,
+    height: VueTypes.number.isRequired,
+    width: VueTypes.number.isRequired,
+  },
+  data() {
+    return {
+      mousePressed: false,
+      lastX: undefined,
+      lastY: undefined,
+      ctx: undefined,
+    };
+  },
+  mounted() {
+    this.ctx = this.$refs.myCanvas.getContext('2d');
+  },
+  methods: {
+    draw(x, y, isDown) {
+      if (isDown) {
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = 'red';
+        this.ctx.lineWidth = 7;
+        this.ctx.lineJoin = 'round';
+        this.ctx.moveTo(this.lastX, this.lastY);
+        this.ctx.lineTo(x, y);
+        this.ctx.closePath();
+        this.ctx.stroke();
+      }
+      this.lastX = x;
+      this.lastY = y;
+    },
+    clearArea() {
+      this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+      this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    },
+    handleMouseDown(e) {
+      this.mousePressed = true;
+      this.draw(e.pageX - this.offset(e.target).left, e.pageY - this.offset(e.target).top, false);
+    },
+    offset(element) {
+      const rect = element.getBoundingClientRect();
+      return {
+        top: rect.top + window.scrollY,
+        left: rect.left + window.scrollX,
+      };
+    },
+    handleMouseMove(e) {
+      if (this.mousePressed) {
+        this.draw(e.pageX - this.offset(e.target).left, e.pageY - this.offset(e.target).top, true);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Board } from './Board/Board.vue';
 export { default as ImageCard } from './ImageCard/ImageCard.vue';
 export { default as Wheel } from './Wheel/Wheel.vue';
 export { default as Notification } from './Notification/Notification.vue';
+export { default as Canvas } from './Canvas/Canvas.vue';
