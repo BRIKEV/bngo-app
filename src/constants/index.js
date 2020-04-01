ae9ea5944725c6a49022d6909662e4a47bd20c17
kevin Martínez <kevinccbsg@gmail.com>
2020-04-01 23:07:46 +0200

    feat: add cookie options for production environment

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,4 +3,12 @@ export const NOT_VALID_VALUES = [null, undefined, '', []];
 
 export const COOKIE_GAME_KEY = 'game_key';
 
-export const cookieOptions = () => ({});
+export const cookieOptions = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return {
+      secure: true,
+      sameSite: 'lax',
+    };
+  }
+  return {};
+};
