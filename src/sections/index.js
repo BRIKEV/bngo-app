5190ffc707ba0650ee20ffe15188a9401baebe8b
Briam Martinez <briammartinez@gmail.com>
2020-04-02 08:34:13 +0200

    Created UserBoardSection

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,3 +1,4 @@
 export { default as JoinGameSection } from './JoinGameSection/JoinGameSection.vue';
 export { default as AccessGameForm } from './AccessGameSection/AccessGameForm.vue';
 export { default as CreateGameForm } from './CreateGameSection/CreateGameForm.vue';
+export { default as UserBoardSection } from './UserBoardSection/UserBoardSection.vue';
