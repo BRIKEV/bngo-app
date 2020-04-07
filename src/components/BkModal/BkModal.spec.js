f52fc7b06aad7fb6ca1ce9bb2a9b66c3ab21d5f9
Briam Martinez <briammartinez@gmail.com>
2020-04-07 19:18:19 +0200

    Added test into BkModal

--- src/components/BkModal/BkModal.spec.js
+++ src/components/BkModal/BkModal.spec.js
@@ -6,4 +6,9 @@ describe('BkModal component', () => {
     const wrapper = mount(BkModal);
     expect(wrapper.element).toMatchSnapshot();
   });
+  it('Should fire close event when click default btn', () => {
+    const wrapper = mount(BkModal);
+    wrapper.find('.modalDefaultButton').trigger('click');
+    expect(wrapper.emitted('close')).toBeTruthy();
+  });
 });
