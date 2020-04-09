ef386c7d41372ad61fcb067cf18628dac48ce4ab
Briam Martinez <briammartinez@gmail.com>
2020-04-09 23:10:16 +0200

    Changes styles of info

--- src/sections/GameActionsSection/GameActionsSection.vue
+++ src/sections/GameActionsSection/GameActionsSection.vue
@@ -104,6 +104,8 @@ $boxShadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
     width: 100%;
     height: 100%;
     overflow: hidden;
+    box-shadow: $boxShadow;
+    border-radius: calculateRem(20px);
   }
   .users {
     position: absolute;
