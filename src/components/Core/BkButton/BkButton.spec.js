5fffb6fd4c30d7e34f5df7804586cee404e94108
Briam Martinez Escobar <briammartinez@gmail.com>
2020-04-10 14:53:08 +0200

    Feature/ui improvements (#47)
    * Created global config of unit tests
    
    * Added global config to BkButton to delete console warning
    
    * Created tests of AccessGameForm section
    
    * Deleted unnecessary validation
    
    * Added method to register our core components for the unit tests
    
    * Added tests of AccessGame and CreateGame sections
    
    * Added unit tests of JoinGameSection

--- src/components/Core/BkButton/BkButton.spec.js
+++ src/components/Core/BkButton/BkButton.spec.js
@@ -1,13 +1,18 @@
 import { mount, shallowMount } from '@vue/test-utils';
 import BkButton from '@/components/Core/BkButton/BkButton.vue';
+import localVue from '@/config/tests';
 
 describe('BkButton component', () => {
+  const defaultConfig = {
+    localVue,
+  };
   it('Should be render correctly', () => {
-    const wrapper = mount(BkButton);
+    const wrapper = mount(BkButton, defaultConfig);
     expect(wrapper.element).toMatchSnapshot();
   });
   it('Should has outline class', () => {
     const wrapper = shallowMount(BkButton, {
+      ...defaultConfig,
       propsData: { outline: true },
     });
     expect(wrapper.element).toMatchSnapshot();
