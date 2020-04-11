64f53cce8f29b0a2dbae1486572520eb817d3ea5
Briam Martinez <briammartinez@gmail.com>
2020-04-11 17:10:58 +0200

    Created basic carrousel component

---
+++ src/components/Carrousel/Carrousel.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Carrousel">
+    <slot name="items" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Carrousel',
+};
+</script>
+<style lang="scss" scoped>
+@import "@/theme/index.scss";
+$boxShadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+
+.Carrousel {
+  display: flex;
+  width: 100%;
+  overflow-x: auto;
+  background: none;
+  > div + div {
+    margin: 0 calculateRem(10px);
+  }
+}
+</style>
