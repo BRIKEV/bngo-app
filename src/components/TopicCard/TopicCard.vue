e66e21300f3a0b4f97f78cc3ab52d22fe95aa0b4
Briam Martinez <briammartinez@gmail.com>
2020-04-11 17:24:54 +0200

    improve hover transition

--- src/components/TopicCard/TopicCard.vue
+++ src/components/TopicCard/TopicCard.vue
@@ -31,23 +31,24 @@ export default {
 $boxShadowTopic: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14),
   0px 1px 5px 0px rgba(0, 0, 0, 0.12);
 $backgroundWithHover: #d2c6b2 !important;
+$textColor: #f4f4f4;
 
 .topicCard {
-  min-width: 342px;
-  max-width: 342px;
-  height: 150px;
+  min-width: calculateRem(342px);
+  max-width: calculateRem(342px);
+  height: calculateRem(150px);
   border-radius: calculateRem(24px) calculateRem(4px);
   display: flex;
   align-items: flex-start;
   justify-content: space-between;
-  padding: 15px;
+  padding: calculateRem(15px);
   box-shadow: $boxShadowTopic;
-  color: #f4f4f4;
+  color: $textColor;
   &:hover {
     cursor: pointer;
     background: $backgroundWithHover;
     color: $dark;
-    transition: background 1s;
+    transition: background .5s;
   }
   .imgContainer {
     width: 35%;
@@ -59,7 +60,7 @@ $backgroundWithHover: #d2c6b2 !important;
     }
   }
   .infoTopic {
-    max-width: 180px;
+    max-width: calculateRem(180px);
     .title {
       padding-bottom: calculateRem(10px);
       font-size: $fs-h4;
