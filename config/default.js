7ff2e7353889406331afe722e665db51b8ea49b2
kevin Martínez <kevinccbsg@gmail.com>
2020-04-12 17:33:41 +0200

    add new data

--- config/default.js
+++ config/default.js
@@ -22,7 +22,7 @@ module.exports = {
     api: {
       frontMainFile: join(__dirname, '..', 'dist', 'index.html'),
       ...tokenOptions,
-      validTopics: ['standard', 'default', 'cars'],
+      validTopics: ['springfield', 'default', 'cars'],
     },
     admin: {
       cloudImages: process.env.NODE_ENV === 'production',
