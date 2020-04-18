687479e2159d7a8122e00195d7889f6d4518e28f
Briam Martinez <briammartinez@gmail.com>
2020-04-18 04:06:58 +0200

    Improve ui styles

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -178,7 +178,7 @@ export default {
   margin-right: auto;
   display: flex;
   height: 100%;
-  margin-top: calculateRem(25px);
+  margin-top: calculateRem(50px);
   margin-bottom: calculateRem(25px);
   .Info {
     display: flex;
